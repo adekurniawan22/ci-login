e8af43ad5155535ca59054fe6f5e917cf0092923
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-03-03 14:15:37 +0700

    update sidebar

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -34,7 +34,7 @@
                     if(password_verify($password,$user['password'])){
                         $data = [
                             'email' => $user['email'],
-                            '$role_id' => $user['role_id']
+                            'role_id' => $user['role_id']
                         ];
 
                         $this->session->set_userdata($data);
